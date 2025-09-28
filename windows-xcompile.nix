0810cb42b973889df171b4c387c79ee76833cfbd
Tim Aschhoff <tim@aschhoff.de>
2025-09-28 19:23:52 +0200

    Allow windows cross compile

---
+++ windows-xcompile.nix
@@ -0,0 +1,10 @@
+with import <nixpkgs> {
+  crossSystem = {
+    config = "x86_64-w64-mingw32";
+  };
+};
+
+mkShell {
+  buildInputs = with pkgs; [ windows.mingw_w64_pthreads
+    windows.pthreads ];
+}
