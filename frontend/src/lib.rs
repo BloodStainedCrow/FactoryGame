325e9e1492e1870fdff6335d7e14ad6d38ebc143
Tim Aschhoff <tim@aschhoff.de>
2026-06-16 21:59:38 +0200

    Rename frontend to world

--- frontend/src/lib.rs
+++ frontend/src/lib.rs
@@ -1,12 +1,1 @@
-#![feature(const_convert)]
-#![feature(const_trait_impl)]
-#![feature(never_type)]
-#![feature(int_roundings)]
 
-mod chunk;
-pub mod entity;
-mod sparse_grid;
-mod surface;
-
-#[cfg(test)]
-mod tests {}
---
+++ world/src/lib.rs
@@ -0,0 +1,12 @@
+#![feature(const_convert)]
+#![feature(const_trait_impl)]
+#![feature(never_type)]
+#![feature(int_roundings)]
+
+mod chunk;
+pub mod entity;
+mod sparse_grid;
+mod surface;
+
+#[cfg(test)]
+mod tests {}
